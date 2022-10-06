e1fdf196d248fa96ab95a1ca14103f45b34ab96d
mertunctuncer <mertunctuncer@gmail.com>
2022-10-06 13:41:52 +0300

    Mob spawning impl

---
+++ src/main/kotlin/dev/peopo/devroomdemo/dungeon/Spawner.kt
@@ -0,0 +1,26 @@
+package dev.peopo.devroomdemo.dungeon
+
+import org.bukkit.Location
+import org.bukkit.entity.Player
+import java.lang.RuntimeException
+import kotlin.random.Random
+
+
+class NoValidPositionException : RuntimeException()
+
+
+fun getMobSpawnPosition(tries: Int, range: Int, player: Player) : Location? {
+	val playerLoc = player.location
+	repeat(tries) {
+		val x = Random.nextInt(playerLoc.blockX - range, playerLoc.blockX + range + 1)
+		val z = Random.nextInt(playerLoc.blockZ - range, playerLoc.blockZ + range + 1)
+		for (y in playerLoc.blockY + 5 downTo playerLoc.blockY - 5) {
+			val currentBlock = playerLoc.world.getBlockAt(x,y,z)
+			val blockAbove = playerLoc.world.getBlockAt(x, y+1, z)
+			val blockTwoAbove = playerLoc.world.getBlockAt(x, y+2, z)
+			if(!currentBlock.isEmpty && blockAbove.isEmpty && blockTwoAbove.isEmpty) return Location(playerLoc.world, x.toDouble() + 0.5,y.toDouble() + 1, z.toDouble() + 0.5)
+		}
+	}
+
+	return null
+}
